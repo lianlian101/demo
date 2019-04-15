7e5df5707773bc6644b8961dbeb1bfb529001f4b
zhb <zhb@192.168.31.126>
2019-04-15 18:22:24 +0800

    master提交

--- src/main/java/com/mvn/test/controller/UserController.java
+++ src/main/java/com/mvn/test/controller/UserController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,8 @@ import com.mvn.test.service.UserService;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    
+    private Logger log = LoggerFactory.getLogger(UserController.class);
     
     @Autowired
     private UserService userService;
