4432ee7ed4ee9aeb1a4c7330b55fc553ff18ed75
zhb <3091556703@qq.com>
2019-05-05 15:15:35 +0800

    idea测试

--- src/main/java/com/mvn/test/controller/UserController.java
+++ src/main/java/com/mvn/test/controller/UserController.java
@@ -1,29 +1,22 @@
 package com.mvn.test.controller;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.mvn.test.entity.User;
 import com.mvn.test.entity.pojo.UserResult;
 import com.mvn.test.service.UserService;
-import com.mvn.test.service.impl.RedisService;
 import com.mvn.test.util.pojo.JsonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user/")
