87267818ae8e48262f74d923610f15301e35d43b
zhb <zhb@DESKTOP-R7KVT9O>
2019-05-15 17:26:11 +0800

    未改动

--- src/main/java/com/mvn/test/config/FilterAndInterceptorAndListenerConfig.java
+++ src/main/java/com/mvn/test/config/FilterAndInterceptorAndListenerConfig.java
@@ -16,7 +16,7 @@ import com.mvn.test.util.listener.SessionListener;
 
 @Configuration
 public class FilterAndInterceptorAndListenerConfig extends WebMvcConfigurerAdapter {
-    
+
     /**
      * 设置默认首页
      */
@@ -28,15 +28,6 @@ public class FilterAndInterceptorAndListenerConfig extends WebMvcConfigurerAdapt
     }
     
     /**
-     * 添加拦截请求和排除拦截请求
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new ReqInterceptor()).addPathPatterns("/**").excludePathPatterns("/login/**");
-        super.addInterceptors(registry);
-    }
-
-    /**
      * 配置静态资源
      */
     @Override
@@ -44,13 +35,24 @@ public class FilterAndInterceptorAndListenerConfig extends WebMvcConfigurerAdapt
         /*
          * springboot默认情况下，resources目录的resources/static/public文件下的文件，
          * 不用加resources/static/public路径可直接访问，如果加了resources/static/
-         * public路径就访问不了了； 但是，如果想加resources/static/public路径也能访问，需要配置以下配置
+         * public路径就访问不了了；
+         * 但是，如果想加resources/static/public路径也能访问，需要配置以下配置；加入配置的路径，可以使用配置路径直接访问，
+         * 相当于静态文件(/file/**)
          */
         // 当访问路径中包含handler中的路径的时候，resource handler就会去locations中的目录下访问了
         String[] handler = { "/resources/**", "/static/**", "/public/**", "/file/**" };
         String[] locations = { "classpath:/resources/", "classpath:/static/", "classpath:/public/", "classpath:/file/" };
         registry.addResourceHandler(handler).addResourceLocations(locations);
         super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 添加拦截请求和排除拦截请求
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ReqInterceptor()).addPathPatterns("/**").excludePathPatterns("/login/**");
+        super.addInterceptors(registry);
     }
 
     /**
@@ -76,5 +78,5 @@ public class FilterAndInterceptorAndListenerConfig extends WebMvcConfigurerAdapt
         registrationBean.setListener(new SessionListener());
         return registrationBean;
     }
- 
+
 }
