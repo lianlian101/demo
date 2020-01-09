0701d5c1186fc389c91b85ed59127757a52adb3b
zhb <zhb@DESKTOP-R7KVT9O>
2020-01-09 16:58:50 +0800

    集合测试调整

--- src/test/java/com/demo/list/ListTest.java
+++ src/test/java/com/demo/list/ListTest.java
@@ -5,11 +5,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -22,29 +20,27 @@ import com.mvn.test.entity.User;
 public class ListTest {
 
     /**
-     * 创建日期: 2019年4月29日 创建人: zhb 说明: Collentions.frequency(list, str);
+     * 创建日期: 2019年4月29日 创建人: zhb 说明: Collentions
      *
      */
     @Test
     public void demo1() {
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("zhangsan");
-        list.add("zhangsan");
-        list.add("lisi");
-        list.add("wangwu");
-        list.add("wangwu");
-        System.out.println(Collections.frequency(list, "wangwu"));
-        ArrayList<String> list2 = new ArrayList<String>();
-        for (int i = 0; i < list.size(); i++) {
-            String name = list.get(i);
-            if (Collections.frequency(list, name) > 1) {
-                list2.add(name);
-            }
-        }
-        HashSet<String> set = new HashSet<>(list2);
-        list2.clear();
-        list2.addAll(set);
-        System.out.println(list2);
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("java","sql","go","java","sql"));
+        
+        // 判断指定元素在集合中出现的次数
+        System.out.println(Collections.frequency(list, "java"));
+        
+        // 判断指定元素是否存在于集合，存在返回第一次出现的位置；不存在返回-1
+        System.out.println(Collections.binarySearch(list, "java"));
+        
+        // 判断是否有相同的元素，没有返回true，有返回false
+        List<Integer> a = new ArrayList<>(Arrays.asList(0,2,3,7,8,9));
+        List<Integer> b = new ArrayList<>(Arrays.asList(0,1,2,3,5,6));
+        System.out.println(Collections.disjoint(a, b));
+        
+        // 返回一个空列表（不可变的），返回的空列表是一个内部类创建的，不可调用add方法，调用报错
+        List<String> emptyList = Collections.emptyList();
+        System.out.println(CollectionUtils.isEmpty(emptyList));
     }
 
     /**
@@ -53,15 +49,12 @@ public class ListTest {
      */
     @Test
     public void demo2() {
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("zhangsan");
-        list.add("zhangsan");
-        list.add("lisi");
-        list.add("wangwu");
-        list.add("wangwu");
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("java","sql","go"));
         String s = JSON.toJSONString(list);
         System.out.println(s);
-        String str = StringUtils.join(list.toArray(), ",");
+        Object[] array = list.toArray();
+        System.out.println(Arrays.asList(array));
+        String str = StringUtils.join(list, ",");
         System.out.println(str);
     }
 
@@ -71,14 +64,7 @@ public class ListTest {
      */
     @Test
     public void demo3() {
-        List<String> list = new ArrayList<String>();
-        list.add("java");
-        list.add("mysql");
-        list.add("sql");
-        list.add("js");
-        list.add("php");
-        list.add("python");
-        list.add("go");
+        List<String> list = new ArrayList<String>(Arrays.asList("java","mysql","sql","js","php","python","go"));
         System.out.println(list);
         System.out.println("迭代的方式遍历：");
         Iterator<String> iterator = list.iterator();
@@ -109,36 +95,11 @@ public class ListTest {
     }
 
     /**
-     * 创建日期: 2019年4月29日 创建人: zhb 说明: list.iterator(); iterator.remove();
-     *
-     */
-    @Test
-    public void demo4() {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        /*for (String item : list) {
-            if ("2".equals(item)) {
-                list.remove(item);
-            }
-        }*/
-        
-        Iterator<String> iterator = list.iterator();
-        while(iterator.hasNext()){
-            String next = iterator.next();
-            if("1".equals(next)){
-                iterator.remove();
-            }
-        }
-        System.out.println(list);
-    }
-    
-    /**
      * 创建日期: 2019年5月28日 创建人: zhb 说明: 根据对象的某个属性过滤
      *
      */
     @Test
-    public void demo5(){
+    public void demo4(){
         User user = new User(1,"张三","123", new Date(),1);
         User user2 = new User(2,"张三","456", new Date(),1);
         User user3 = new User(3,"李四","123", new Date(),1);
@@ -154,49 +115,13 @@ public class ListTest {
     }
     
     /**
-     * 创建日期: 2019年6月26日 创建人: zhb 说明: 迭代测试
-     *
-     */
-    @Test
-    public void demo6(){
-        String[] ss = {"java","mysql","js","java","java"};
-        List<String> list = Arrays.asList(ss);
-        Iterator<String> iterator = list.iterator();
-        if(iterator.hasNext()){
-            while (iterator.hasNext()) {
-                System.out.println(iterator.next());
-            }
-        }
-    }
-    
-    /**
-     * 创建日期: 2019年6月26日 创建人: zhb 说明: 判断数据是否存在集合中
-     *
-     */
-    @Test
-    public void demo7(){
-        Set<Long> set = new HashSet<Long>();
-        set.add(12L);
-        set.add(300L);
-        set.add(90L);
-        
-        Set<Long> set2 = new HashSet<Long>();
-        set2.add(12L);
-        set2.add(300L);
-        set2.add(20L);
-        
-        boolean b = set.contains(300L);
-        System.out.println(b);
-    }
-    
-    /**
      * 日期：2019年8月22日
      * 作者：zhb
      * 说明：向集合中添加对象，对象更改，集合中的元素是否改变
      * 
      */
     @Test
-    public void demo08(){
+    public void demo05(){
         User user = new User();
         user.setId(1);
         user.setUsername("张三");
@@ -218,7 +143,7 @@ public class ListTest {
      * 
      */
     @Test
-    public void demo09(){
+    public void demo06(){
         // 根据条件过滤
         List<Integer> filterNums = new ArrayList<>(Arrays.asList(1,3,5,7,9));
         // 移除符合条件的元素
@@ -239,7 +164,7 @@ public class ListTest {
      * 
      */
     @Test
-    public void demo10(){
+    public void demo7(){
         List<Integer> a = new ArrayList<>(Arrays.asList(0,2,3,7,8,9));
         List<Integer> b = new ArrayList<>(Arrays.asList(0,1,2,3,5,6));
         // 并集
@@ -251,31 +176,6 @@ public class ListTest {
         // 差集
         List<Integer> disjunction = CollectionUtils.disjunction(a, b).stream().collect(Collectors.toList());
         System.out.println(disjunction);
-    }
-    
-    /**
-     * 日期：2020年1月7日
-     * 作者：zhb
-     * 说明：Collections
-     * 
-     */
-    @Test
-    public void demo11(){
-        List<String> list = new ArrayList<>(Arrays.asList("java","sql","go","java"));
-        // 判断指定元素是否存在于集合，存在返回第一次出现的位置；不存在返回-1
-        System.out.println(Collections.binarySearch(list, "java"));
-        
-        // 判断是否有相同的元素，没有返回true，有返回false
-        List<Integer> a = new ArrayList<>(Arrays.asList(0,2,3,7,8,9));
-        List<Integer> b = new ArrayList<>(Arrays.asList(0,1,2,3,5,6));
-        System.out.println(Collections.disjoint(a, b));
-        
-        // 判断指定元素在集合中出现的次数
-        System.out.println(Collections.frequency(list, "java"));
-        
-        // 返回一个空列表（不可变的），返回的空列表是一个内部类创建的，不可调用add方法，调用报错
-        List<String> emptyList = Collections.emptyList();
-        System.out.println(CollectionUtils.isEmpty(emptyList));
     }
     
     
